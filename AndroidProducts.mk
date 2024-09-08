3438e576ebdb988d431197ca3c59d0a87fa57791
SharmagRit <sharmagritstuff@gmail.com>
2024-09-08 09:09:59 +0200

    marble: It's `RisingOS`
    * Revert commit: 2ba71de2641918a512e96bd801d6f581d7b5fa69

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,3 +6,8 @@
 
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_marble.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_marble-user \
+    lineage_marble-userdebug \
+    lineage_marble-eng
--- lineage_marble.mk
+++ lineage_marble.mk
@@ -7,14 +7,15 @@
 # Inherit from marble device
 $(call inherit-product, device/xiaomi/marble/device.mk)
 
-# Inherit from common lineage configuration
+# Inherit from common RisingOS configuration
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_marble
-PRODUCT_DEVICE := marble
-PRODUCT_MANUFACTURER := Xiaomi
+# Device identifier
 PRODUCT_BRAND := POCO
+PRODUCT_DEVICE := marble
 PRODUCT_MODEL := 23049PCD8G
+PRODUCT_NAME := lineage_marble
+PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_SYSTEM_NAME := marble_global
 PRODUCT_SYSTEM_DEVICE := marble
@@ -27,3 +28,16 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT := POCO/marble_global/marble:14/UKQ1.230804.001/V816.0.8.0.UMRMIXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# RisingOS stuff
+TARGET_PREBUILT_LAWNCHAIR_LAUNCHER := true
+TARGET_DEFAULT_PIXEL_LAUNCHER := true
+TARGET_ENABLE_BLUR := true
+WITH_GMS := true
+
+# RisingOS Maintainer stuff
+RISING_MAINTAINER=SharmagRit
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    RISING_CHIPSET="Snapdragon® 7+ Gen 2" \
+    RISING_MAINTAINER="SharmagRit"
