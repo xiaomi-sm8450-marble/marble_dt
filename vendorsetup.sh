017ac588449debcc1b7f0a97fbb8df8dbdbebedf
SharmagRit <sharmagritstuff@gmail.com>
2024-10-06 07:28:36 +0200

    marble: Switch to branch `14`

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,28 +5,28 @@ echo 'Nuke Source stuffs'
 	rm -rf hardware/xiaomi
 
 echo 'Cloning Basic Call Recorder Tree'
-	git clone https://github.com/xiaomi-sm8450-marble/android_vendor_bcr -b main vendor/bcr
+	git clone https://github.com/xiaomi-sm8450-marble/android_vendor_bcr -b 14 vendor/bcr
 
 echo 'Cloning Device Common Tree'
-	git clone https://github.com/xiaomi-sm8450-marble/android_device_xiaomi_sm8450-common -b main device/xiaomi/sm8450-common
+	git clone https://github.com/xiaomi-sm8450-marble/android_device_xiaomi_sm8450-common -b 14 device/xiaomi/sm8450-common
 
 echo 'Cloning MemeCamera Tree'
-	git clone https://github.com/xiaomi-sm8450-marble/android_device_xiaomi_miuicamera-marble.git -b main device/xiaomi/miuicamera-marble
-	git clone https://gitlab.com/xiaomi-sm8450-marble/android_vendor_xiaomi_miuicamera-marble.git -b main vendor/xiaomi/miuicamera-marble
+	git clone https://github.com/xiaomi-sm8450-marble/android_device_xiaomi_miuicamera-marble.git -b 14 device/xiaomi/miuicamera-marble
+	git clone https://gitlab.com/xiaomi-sm8450-marble/android_vendor_xiaomi_miuicamera-marble.git -b 14 vendor/xiaomi/miuicamera-marble
 
 echo 'Cloning Hardware Display Tree'
-	git clone https://github.com/xiaomi-sm8450-marble/android_hardware_qcom_display -b main hardware/qcom-caf/sm8450/display
+	git clone https://github.com/xiaomi-sm8450-marble/android_hardware_qcom_display -b 14 hardware/qcom-caf/sm8450/display
 
 echo 'Cloning Hardware Xiaomi Tree'
-	git clone https://github.com/xiaomi-sm8450-marble/android_hardware_xiaomi -b main hardware/xiaomi
+	git clone https://github.com/xiaomi-sm8450-marble/android_hardware_xiaomi -b 14 hardware/xiaomi
 
 echo 'Cloning Kernel Tree'
-	git clone --depth=1 https://github.com/xiaomi-sm8450-marble/android_kernel_xiaomi_sm8450 -b main kernel/xiaomi/sm8450
-	git clone --depth=1 https://github.com/xiaomi-sm8450-marble/android_kernel_xiaomi_sm8450-modules -b main kernel/xiaomi/sm8450-modules
-	git clone --depth=1 https://github.com/xiaomi-sm8450-marble/android_kernel_xiaomi_sm8450-devicetrees -b main kernel/xiaomi/sm8450-devicetrees
+	git clone --depth=1 https://github.com/xiaomi-sm8450-marble/android_kernel_xiaomi_sm8450 -b 14 kernel/xiaomi/sm8450
+	git clone --depth=1 https://github.com/xiaomi-sm8450-marble/android_kernel_xiaomi_sm8450-modules -b 14 kernel/xiaomi/sm8450-modules
+	git clone --depth=1 https://github.com/xiaomi-sm8450-marble/android_kernel_xiaomi_sm8450-devicetrees -b 14 kernel/xiaomi/sm8450-devicetrees
 
 echo 'Cloning Vendor Tree'
-	git clone https://github.com/xiaomi-sm8450-marble/android_vendor_xiaomi_marble -b main vendor/xiaomi/marble
+	git clone https://github.com/xiaomi-sm8450-marble/android_vendor_xiaomi_marble -b 14 vendor/xiaomi/marble
 
 echo 'Cloning Vendor Common Tree'
-	git clone https://github.com/xiaomi-sm8450-marble/android_vendor_xiaomi_sm8450-common -b main vendor/xiaomi/sm8450-common
+	git clone https://github.com/xiaomi-sm8450-marble/android_vendor_xiaomi_sm8450-common -b 14 vendor/xiaomi/sm8450-common
